45f679584f7becf97914ee0e80b5cf7e019fc65f
Chen-dzl <15871499054@163.com>
2025-05-02 11:10:06 +0800

    更新代码

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main() {
 	double dx;//Discrete distance
 	int N;//Number of discrete points
 	N = 31;
-	vector<double> x(N, 0), A(N, 0), Ma(N, 1);//Distance Function
+	vector<double> x(N, 0), A(N, 0), Ma(N, 0);//Distance Function
 
 	L = 3.0;
 	dx = L / (N - 1);
@@ -36,15 +36,23 @@ int main() {
 	{
 		x[i] = dx * i;
 		A[i] = 1 + 2.2 * pow(x[i] - 1.5, 2);
+		if (i < (N - 1) / 2)
+		{
+			Ma[i] = 0.2;
+		}
+		else
+		{
+			Ma[i] = 4.0;
+		}
 	}
-
-	/*	Ma = {
+	/*
+		Ma = {
 		 0.098,0.11,0.124,0.14,0.16,0.185,0.214,0.249,0.293,0.347,
 		 0.413,0.494,0.592,0.709,0.846,1,1.169,1.348,1.531,1.715,1.896,
 		 2.071,2.24,2.402,2.557,2.706,2.848,2.983,3.114,3.239,3.359
-		};Analytical solution with three decimal places retained*/
-
-		//Solve point by point
+		};//Analytical solution with three decimal places retained
+	*/
+	//Solve point by point
 	ofstream outFile("C:\\Desktop\\CDNM.txt");
 	for (int i = 0; i < N; i++)
 	{
